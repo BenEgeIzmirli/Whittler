cf8788153e14fd9fc5dcaaa78028bb7564f62f7a
Paul Houser <m.houser.p@gmail.com>
2021-08-25 17:54:30 -0700

    OS agnostic (not really) way to open files

--- Whittler/modules/trufflehog.py
+++ Whittler/modules/trufflehog.py
@@ -1,4 +1,5 @@
 from Whittler.classes.Result import Result
+from charset_normalizer import from_path
 import re
 import json
 
@@ -6,7 +7,7 @@ import json
 class TrufflehogResult(Result):
 
     FRIENDLY_NAME = "trufflehog"
-    
+
     ATTRIBUTES = [
         "branch",
         "commit",
@@ -21,7 +22,8 @@ class TrufflehogResult(Result):
 
     @staticmethod
     def give_result_dict_list(fname):
-        with open(fname,"r", encoding=TrufflehogResult.Config.FILE_ENCODING) as f:
+        encoding = from_path(fname).best().encoding
+        with open(fname,"r", encoding=encoding) as f:
             output_raw = f.read()
             # For some reason, trufflehog just tacks together JSON objects without commas or an array to contain them,
             # e.g. {blah:blah} {blah:blah} . We want to turn that into [{blah:blah},{blah:blah}] .
